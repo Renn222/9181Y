b429deb613588f7de20557663f891fcb4a79e51b
Jaimin Nimavat <jaimin2468@gmail.com>
2018-12-17 11:10:24 -0800

    trying to add main controller

---
+++ include/members.hpp
@@ -0,0 +1,18 @@
+#ifndef _MEMBERS_HPP
+#define _MEMBERS_HPP
+
+#include "main.h"
+
+// Ports
+static pros::Motor * backLeftDrive;
+static pros::Motor * frontLeftDrive;
+static pros::Motor * intakeMotor;
+static pros::Motor * frontLauncherMotor;
+static pros::Motor * backLauncherMotor;
+static pros::Motor * liftMotor;
+static pros::Motor * frontRightDrive;
+static pros::Motor * backRightDrive;
+
+controllerMain = new pros::Controller(CONTROLLER_MAIN);
+
+#endif
